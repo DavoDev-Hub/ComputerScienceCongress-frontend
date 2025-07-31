726a1e6f0c8021bb41e6fdf9643686e1b23b1bb6
DavoDev-Hub <juanpajmz13@gmail.com>
2025-07-31 13:46:23 -0600

    dashboard fixed

--- src/services/adminServices/apiDashboard.ts
+++ src/services/adminServices/apiDashboard.ts
@@ -4,7 +4,7 @@ const API_BASE = `${import.meta.env.VITE_API_URL}/admin`
 
 
 export const getDashboardData = async (): Promise<DashboardData> => {
-    const res = await api.get(`${API_BASE}/admin/dashboard`)
+    const res = await api.get(`${API_BASE}/dashboard`)
     return res.data
 }
 
