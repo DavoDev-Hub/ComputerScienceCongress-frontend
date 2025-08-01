39f3f5fe4029d6c39cf642be57e1791ee1055b61
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-01 13:47:41 -0600

    footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,16 @@
 import { siGithub, siInstagram, siFacebook } from "simple-icons"
+
 export default function Footer() {
     return (
         <footer className="relative z-10 bg-[#002E5D]/90 backdrop-blur-md text-white py-8 mt-auto">
             <div className="container mx-auto px-4 text-center">
                 <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
                     <div className="text-sm">
-                        Creado por Juan Pablo Jimenez Martin - &copy; {new Date().getFullYear()} Universidad Autónoma de Aguascalientes. Todos los derechos reservados.
+                        Creado por Juan Pablo Jimenez Martin - &copy; {new Date().getFullYear()} Universidad Autónoma de
+                        Aguascalientes. Todos los derechos reservados.
                     </div>
                     <div className="flex space-x-6">
-                        <a href="#" aria-label="GitHub" className="hover:text-blue-300 transition-colors">
+                        <a href="https://github.com/DavoDev-Hub/" target="_blank" aria-label="GitHub" className="hover:text-blue-300 transition-colors">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 role="img"
@@ -21,8 +23,7 @@ export default function Footer() {
                                 <path d={siGithub.path} />
                             </svg>
                         </a>
-
-                        <a href="#" aria-label="Instagram" className="hover:text-blue-300 transition-colors">
+                        <a href="https://www.instagram.com/uaa_mx/" target="_blank" aria-label="Instagram" className="hover:text-blue-300 transition-colors">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 role="img"
@@ -35,8 +36,7 @@ export default function Footer() {
                                 <path d={siInstagram.path} />
                             </svg>
                         </a>
-
-                        <a href="#" aria-label="Facebook" className="hover:text-blue-300 transition-colors">
+                        <a href="https://www.facebook.com/uaa.mx/" target="_blank" aria-label="Facebook" className="hover:text-blue-300 transition-colors">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 role="img"
